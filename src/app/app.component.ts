45da45a9670db6106253aa44e982d4a38445a032
Matías Lobo <matiasnl.1993@gmail.com>
2019-08-14 22:05:02 -0300

    Preparación del layout principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'organizador';
+export class AppComponent implements AfterViewInit {
+
+  constructor() {  }
+  
+  // Menúes
+  @ViewChild('menu', { static: false }) menu: MatSidenav;
+  @ViewChild('extra', { static: false }) extra: MatSidenav;
+
+  menuIsOpen: boolean;
+  extraIsOpen: boolean;
+
+  menuItems: MenuItems[];
+
+  ngAfterViewInit() {
+    this.prepareMenu();
+
+    this.menu.open();
+  }
+
+  async toggleMenu() {
+    this.menuIsOpen = await this.menu.toggle() === 'open' ? true : false;
+  }
+  
+  async toggleExtraMenu() {
+    this.extraIsOpen = await this.extra.toggle() == 'open' ? true : false;
+  }
+
+  prepareMenu() {
+    this.menuItems = [
+      { name: 'Inicio', icon: 'home', link: 'test' },
+      { name: 'Tareas', icon: 'view_list', link: 'test' },
+      { name: 'Proyectos', icon: 'assignment', link: 'test' },
+    ]
+  }
+}
+
+interface MenuItems {
+  name: string;
+  icon?: string;
+  link?: string;
 }
